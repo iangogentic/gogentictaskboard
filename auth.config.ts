33114a85d4ec8b86df7d417d21a4163ffee86a98
iangogentic <ian@gogentic.ai>
2025-09-12 15:11:34 -0500

    Remove Credentials provider to fix Edge Function size limit

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,6 @@
 import type { NextAuthConfig } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
 import Google from "next-auth/providers/google"
 import GitHub from "next-auth/providers/github"
-import bcrypt from "bcryptjs"
-import prisma from "@/lib/prisma"
 
 export default {
   providers: [
@@ -15,43 +12,5 @@ export default {
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
-    Credentials({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          return null
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email as string
-          }
-        })
-
-        if (!user || !user.password) {
-          return null
-        }
-
-        const passwordsMatch = await bcrypt.compare(
-          credentials.password as string,
-          user.password
-        )
-
-        if (!passwordsMatch) {
-          return null
-        }
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          role: user.role,
-        }
-      }
-    })
   ],
 } satisfies NextAuthConfig
